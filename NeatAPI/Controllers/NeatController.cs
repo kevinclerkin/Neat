898da6d9ec7a4110e9ab5daa6b23c3a51a5c4736
KevC <kevin.clerkin1@gmail.com>
2023-10-02 22:36:35 +0100

    Added Controller and NeatRepo class to test

---
+++ NeatAPI/Controllers/NeatController.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+using NeatAPI.Models;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NeatAPI.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class NeatController : ControllerBase
+  {
+    private readonly NeatRepo _repo;
+
+    public NeatController(NeatRepo repo)
+    {
+      _repo = repo;
+    }
+
+
+    // GET: api/<NeatController>
+    [HttpGet]
+    public ActionResult<IEnumerable<Neat>> Get()
+    {
+      var neatList = _repo.GetAll();
+      return Ok(neatList);
+    }
+
+    // GET api/<NeatController>/5
+    //[HttpGet("{id}")]
+    //public string Get(int id)
+    //{
+      //return "value";
+    //}
+
+    // POST api/<NeatController>
+    //[HttpPost]
+    //public void Post([FromBody] string value)
+    //{
+    //}
+
+    // PUT api/<NeatController>/5
+    //[HttpPut("{id}")]
+    //public void Put(int id, [FromBody] string value)
+    //{
+    //}
+
+    // DELETE api/<NeatController>/5
+    //[HttpDelete("{id}")]
+    //public void Delete(int id)
+    //{
+    //}
+  }
+}
--- NeatAPI/Models/Neat.cs
+++ NeatAPI/Models/Neat.cs
@@ -1,7 +1,8 @@
-﻿namespace NeatAPI.Models
+namespace NeatAPI.Models
 {
     public class Neat
     {
+        public int Id { get; set; }
         public String Service { get; set; }
 
         public String Available { get; set; }
@@ -12,4 +13,24 @@
 
         public String ClientEmail { get; set; }
     }
+
+  public class NeatRepo
+  {
+    private static List<Neat> _neatList = new List<Neat>()
+    {
+      new Neat { Id = 1, Service= "Dry Cut", Available = "Dave", DateTime = DateTime.Now, ClientName="Ronan Byrne", ClientEmail= "ronan.byrne@gmail.com"},
+      new Neat { Id = 2, Service= "Beard Trim", Available = "Paul", DateTime = DateTime.Now, ClientName="Paul O'Neill", ClientEmail= "paul.oneill@gmail.com"},
+      new Neat { Id = 1, Service= "Dry Cut & Shave", Available = "Dave", DateTime = DateTime.Now, ClientName="Pat Collins", ClientEmail= "pat.collins@gmail.com"},
+      new Neat { Id = 1, Service= "Dry Cut", Available = "Ryan", DateTime = DateTime.Now, ClientName="Jason Davis", ClientEmail= "jason.davis@gmail.com"}
+    };
+
+    public IEnumerable<Neat> GetAll()
+    {
+      return _neatList;
+
+    }
+
+
+
+  }
 }
