cbfc436e8e13228c40961d769beed8dfc247344f
KevC <kevin.clerkin1@gmail.com>
2023-10-02 21:39:27 +0100

    Added model class

---
+++ NeatAPI/Models/Neat.cs
@@ -0,0 +1,15 @@
+﻿namespace NeatAPI.Models
+{
+    public class Neat
+    {
+        public String Service { get; set; }
+
+        public String Available { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public String ClientName { get; set; }
+
+        public String ClientEmail { get; set; }
+    }
+}
