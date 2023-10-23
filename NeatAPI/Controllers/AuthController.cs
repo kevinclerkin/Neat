daf15fb3e39c16491ca59b76b7420ac30a580bb1
KevC <kevin.clerkin1@gmail.com>
2023-10-23 21:37:33 +0100

    Added Register method & CreatePasswordHash methods to auth controller

--- NeatAPI/Controllers/AuthController.cs
+++ NeatAPI/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NeatAPI.Models;
+using System.Security.Cryptography;
 
 namespace NeatAPI.Controllers
 {
@@ -8,6 +9,28 @@ namespace NeatAPI.Controllers
   [ApiController]
   public class AuthController : ControllerBase
   {
+    public static User user = new User();
+
+    [HttpPost("register")]
+    public async Task<ActionResult<User>> Register(UserDto userDto)
+    {
+      CreatePasswordHash(userDto.Password, out byte[] passwordHash, out byte[] passwordSalt);
+      user.UserName = userDto.UserName;
+      user.PasswordHash = passwordHash;
+      user.PasswordSalt = passwordSalt;
+
+      return Ok(user);
+
+    }
+
+    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+      using(var hmac = new HMACSHA512())
+      {
+        passwordSalt = hmac.Key;
+        passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+      }
+    }
     
   }
 
