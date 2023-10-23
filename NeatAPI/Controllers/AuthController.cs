9e0bfac508de4e1f226b6a40567a09faa4cb2656
KevC <kevin.clerkin1@gmail.com>
2023-10-23 23:36:58 +0100

    Added CreateToken method to auth controller & modified user model

--- NeatAPI/Controllers/AuthController.cs
+++ NeatAPI/Controllers/AuthController.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using NeatAPI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 
 namespace NeatAPI.Controllers
@@ -10,6 +13,12 @@ namespace NeatAPI.Controllers
   public class AuthController : ControllerBase
   {
     public static User user = new User();
+    private readonly IConfiguration _configuration;
+
+    public AuthController(IConfiguration configuration)
+    {
+      _configuration = configuration; 
+    }
 
     [HttpPost("register")]
     public async Task<ActionResult<User>> Register(UserDto userDto)
@@ -36,7 +45,32 @@ namespace NeatAPI.Controllers
         return BadRequest();
       }
 
-      return Ok("Token");
+      string token = CreateToken(user);
+
+      return Ok(token);
+    }
+
+    private string CreateToken(User user)
+    {
+      List<Claim> claims = new List<Claim>
+      {
+        new Claim(ClaimTypes.Name, user.UserName)
+      };
+
+      var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+        _configuration.GetSection("AppSettings:Token").Value));
+
+      var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+      var token = new JwtSecurityToken(
+        claims: claims,
+        expires: DateTime.Now.AddDays(1),
+        signingCredentials: creds);
+
+      var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+      return jwt;
+      
     }
 
     private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
--- NeatAPI/Models/User.cs
+++ NeatAPI/Models/User.cs
@@ -2,14 +2,13 @@ namespace NeatAPI.Models
 {
   public class User
   {
-    public int Id { get; set; }
+    
     public string UserName { get; set; } = string.Empty;
 
     public byte[] PasswordHash { get; set; }
 
     public byte[] PasswordSalt { get; set; }
 
-    public string Email { get; set; } = string.Empty;
 
    
   }
