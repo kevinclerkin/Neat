4ece405f6ccda7920fc16e6f7ddbcc7c6fbfdf8d
KevC <kevin.clerkin1@gmail.com>
2023-10-25 23:41:20 +0100

    Added register method to frontend auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../Models/User';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  registerApiUrl: string = "https://localhost:7193/api/Auth/register";
+
+  constructor(private http: HttpClient) { }
+
+  register(user: User): Observable<any>{
+    return this.http.post<any>(this.registerApiUrl, user);
+  }
 }
