0c79a44ab86ab9f1df9432984109031e4fa81eb2
KevC <kevin.clerkin1@gmail.com>
2023-10-25 23:50:05 +0100

    Added login method to frontend auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,10 +9,15 @@ import { Observable } from 'rxjs';
 export class AuthService {
 
   registerApiUrl: string = "https://localhost:7193/api/Auth/register";
-
+  loginApiUrl: string = "https://localhost:7193/api/Auth/login";
+  
   constructor(private http: HttpClient) { }
 
   register(user: User): Observable<any>{
     return this.http.post<any>(this.registerApiUrl, user);
+  }
+
+  login(user: User): Observable<string>{
+    return this.http.post(this.loginApiUrl, user, {responseType: "text"});
   }
 }
