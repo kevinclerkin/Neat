1270c743486e0d2f37da79f1901f51d702dbd6ce
KevC <kevin.clerkin1@gmail.com>
2023-11-08 01:35:01 +0000

    Added bookings option to navbar component & path to app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegLoginComponent } from './components/reg-login/reg-login.component';
 import { HomeComponent } from './components/home/home.component';
+import { BookingsComponent } from './components/bookings/bookings.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'register', component: RegLoginComponent},
-  {path: 'login', component: RegLoginComponent}
+  {path: 'login', component: RegLoginComponent},
+  {path: 'bookings', component: BookingsComponent}
 ];
 
 @NgModule({
