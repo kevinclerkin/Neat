72765f66643b2db3d0985f8f6c79026fa824be44
KevC <kevin.clerkin1@gmail.com>
2023-11-13 21:58:56 +0000

    Added admin component to routing & navbar link; modified routing & angular.json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegLoginComponent } from './components/reg-login/reg-login.component';
 import { HomeComponent } from './components/home/home.component';
 import { BookingsComponent } from './components/bookings/bookings.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'register', component: RegLoginComponent},
   {path: 'login', component: RegLoginComponent},
-  {path: 'bookings', component: BookingsComponent}
+  {path: 'bookings', component: BookingsComponent},
+  {path: 'admin', component: AdminComponent}
 ];
 
 @NgModule({
