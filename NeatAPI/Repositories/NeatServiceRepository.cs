cae2e1d635a321b40448b7521208a819ab94d5f8
KevC <kevin.clerkin1@gmail.com>
2023-11-20 23:01:01 +0000

    Added get, create, delete methods to neat service repo

---
+++ NeatAPI/Repositories/NeatServiceRepository.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using NeatAPI.Data;
+using NeatAPI.Interfaces;
+using NeatAPI.Models;
+
+namespace NeatAPI.Repositories
+{
+  public class NeatServiceRepository : INeatServiceRepository
+  {
+    private readonly DataContext _context;
+
+    public NeatServiceRepository(DataContext context)
+    {
+        _context = context;
+    }
+    public Service CreateService(Service service)
+    {
+      _context.Services.Add(service);
+      _context.SaveChanges();
+      return service;
+    }
+
+    public IEnumerable<Service> DeleteService(int id)
+    {
+      var deleteService = _context.Services.FirstOrDefault(b => b.ServiceId == id);
+
+      _context.Services.Remove(deleteService);
+      _context.SaveChanges();
+      return _context.Services;
+    }
+
+    public ICollection<Service> GetAllServices()
+    {
+      return _context.Services.OrderBy(a => a.ServiceId).ToList();
+    }
+  }
+}
