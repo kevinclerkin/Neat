0cc2908e005f790b904059ff35c851ba580ef74b
KevC <kevin.clerkin1@gmail.com>
2023-11-20 22:20:45 +0000

    Added actions/requests - get, create, delete - to availability controller

--- NeatAPI/Controllers/AvailabilityController.cs
+++ NeatAPI/Controllers/AvailabilityController.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NeatAPI.Data;
+using NeatAPI.Interfaces;
+using NeatAPI.Models;
+using NeatAPI.Repositories;
 
 namespace NeatAPI.Controllers
 {
@@ -8,12 +11,55 @@ namespace NeatAPI.Controllers
   [ApiController]
   public class AvailabilityController : ControllerBase
   {
-    private readonly DataContext _context;
-    public AvailabilityController(DataContext context)
+    private readonly IAvailabilityRepository _availabilityRepository;
+
+    public AvailabilityController(IAvailabilityRepository availabilityRepository)
     {
-      _context = context;
+      _availabilityRepository = availabilityRepository;
     }
 
-    
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<NeatBooking>), 200)]
+    public IActionResult GetAvailabilites()
+    {
+      var allAvailabilities = _availabilityRepository.GetAvailabilities();
+
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      return Ok(allAvailabilities);
+    }
+
+    [HttpGet("{UserId}")]
+    [ProducesResponseType(typeof(IEnumerable<NeatBooking>), 200)]
+    public IActionResult GetAvailabilityById(int id)
+    {
+      var availabilityByUser = _availabilityRepository.GetAvailabilityByUser(id);
+
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      return Ok(availabilityByUser);
+
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(NeatBooking), 200)]
+    public IActionResult CreateBooking([FromBody] Availability availability)
+    {
+      if (availability == null || !ModelState.IsValid)
+      {
+        return BadRequest("Invalid data; refer to schema");
+      }
+
+      _availabilityRepository.CreateAvailability(availability);
+      return Ok(availability);
+    }
+
+
   }
 }
