ff8f6a122a0aad1b1e9e937f796728a3e36106d7
KevC <kevin.clerkin1@gmail.com>
2023-11-27 17:35:07 +0000

    Added team member model & token dto class to api

---
+++ NeatAPI/TokenDto.cs
@@ -0,0 +1,9 @@
+namespace NeatAPI
+{
+  public class TokenDto
+  {
+    public string AccessToken { get; set; } = string.Empty;
+
+    public string RefreshToken { get; set; } = string.Empty;
+  }
+}
