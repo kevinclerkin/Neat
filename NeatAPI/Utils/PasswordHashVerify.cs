a16755faf7a8076628fe18f7a308879d3d93c5e9
KevC <kevin.clerkin1@gmail.com>
2023-11-29 19:10:08 +0000

    Replaced RNGCryptoServiceProvider with RandomNumberGenerator

--- NeatAPI/Utils/PasswordHashVerify.cs
+++ NeatAPI/Utils/PasswordHashVerify.cs
@@ -4,15 +4,17 @@ namespace NeatAPI
 {
   public class PasswordHashVerify
   {
-    private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+
+    private static readonly RandomNumberGenerator rng;
     private static readonly int SaltSize = 16;
     private static readonly int HashSize = 20;
     private static readonly int Iterations = 10000;
+    
 
     public static string HashPassword(string password)
     {
       byte[] salt;
-      rngCsp.GetBytes(salt = new byte[SaltSize]);
+      rng.GetBytes(salt = new byte[SaltSize]);
 
       var key = new Rfc2898DeriveBytes(password, salt, Iterations);
       var hash = key.GetBytes(HashSize);
