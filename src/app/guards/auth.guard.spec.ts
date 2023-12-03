86427362952fb1621c475b226acff8f7b39c98ed
Kevin Clerkin <kevin.clerkin1@gmail.com>
2023-12-03 17:53:05 +0000

    fixed auth guard test bed

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -1,17 +1,41 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, ActivatedRouteSnapshot, RouterStateSnapshot, Router  } from '@angular/router';
 
-import { authGuard } from './auth.guard';
+import { AuthGuard } from './auth.guard';
+import { NewAuthService } from '../services/new-auth.service';
+import { NgToastService } from 'ng-angular-popup';
 
-describe('authGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+describe('AuthGuard', () => {
+  let executeGuard: CanActivateFn;
+  let authService: NewAuthService;
+  let router: Router;
+  let toastService: NgToastService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        AuthGuard,
+        NewAuthService,
+        NgToastService,
+        Router,
+        
+      ],
+    });
+
+    executeGuard = (...guardParameters) => 
+      TestBed.inject(AuthGuard).canActivate(
+        {} as ActivatedRouteSnapshot,
+        {} as RouterStateSnapshot
+      );
+
+    authService = TestBed.inject(NewAuthService);
+    router = TestBed.inject(Router);
+    toastService = TestBed.inject(NgToastService);
   });
 
   it('should be created', () => {
     expect(executeGuard).toBeTruthy();
   });
+
+
 });
