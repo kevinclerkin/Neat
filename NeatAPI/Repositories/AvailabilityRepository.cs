602b8a8692ec3aa9ff1e1febf2ea572ce8e50c3b
Kevin Clerkin <kevin.clerkin1@gmail.com>
2023-12-05 21:52:43 +0000

    Modified get availability by user

--- NeatAPI/Repositories/AvailabilityRepository.cs
+++ NeatAPI/Repositories/AvailabilityRepository.cs
@@ -25,8 +25,8 @@ namespace NeatAPI.Repositories
 
     public ICollection<Availability> GetAvailabilityByUser(int id)
     {
-            //return _context.Availabilities.Where(a => a.Id == id).ToList();
-            return null;
+            return _context.Availabilities.Where(a => a.TeamMemberId == id).ToList();
+            
     }
 
 
